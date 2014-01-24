69f4f1c961417f447e9dddb66c02c69215abd5f0
AkiraSatou <akirasatou@AkiraSatou-no-MacBook-Pro.local>
2014-01-25 03:00:37 +0900

    Initial Commit

---
+++ XcodeGitSample/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  XcodeGitSample
+//
+//  Created by AkiraSatou on 2014/01/25.
+//  Copyright (c) 2014年 sato.akira. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
